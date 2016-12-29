def13149e00f1e6d948e8a4144d6cc764feba9a9
Adam Tobey <adamtobey@gmail.com>
2016-12-28 19:37:56 -0500

    successfully storing and displaying errors in gutter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import TopBar from './fragments/TopBar';
+import ErrorGutter from './fragments/ErrorGutter';
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App"></div>
+			<div className="App">
 				<TopBar />
+				<ErrorGutter />
 				{this.props.children}
 			</div>
 		);
